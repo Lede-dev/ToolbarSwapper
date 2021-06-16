caf469a7d030eb6731a071ab58ff8b55637320f1
Youtube-Lede <68679956+Youtube-Lede@users.noreply.github.com>
2021-06-16 12:01:12 +0900

    remove cancellable
    remove cancellable from ToolbarScrollEvent

--- src/me/lede/toolbarswapper/event/ToolbarScrollEvent.java
+++ src/me/lede/toolbarswapper/event/ToolbarScrollEvent.java
@@ -1,23 +1,19 @@
 package me.lede.toolbarswapper.event;
 
 import org.bukkit.entity.Player;
-import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public class ToolbarScrollEvent extends Event implements Cancellable {
+public class ToolbarScrollEvent extends Event {
 
 	private final Player player;
 	private final int prevSlot;
 	private final int newSlot;
-	
-	private boolean cancelled;
 
 	public ToolbarScrollEvent(Player player, int prevSlot, int newSlot) {
 		this.player = player;
 		this.prevSlot = prevSlot;
 		this.newSlot = newSlot;
-		this.cancelled = false;
 	}
 	
 	public Player getPlayer() {
@@ -30,16 +26,6 @@ public class ToolbarScrollEvent extends Event implements Cancellable {
 	
 	public int getNewSlot() {
 		return newSlot;
-	}
-
-	@Override
-	public boolean isCancelled() {
-		return cancelled;
-	}
-	
-	@Override
-	public void setCancelled(boolean cancel) {
-		this.cancelled = cancel;
 	}
 
 	private static final HandlerList HANDLERS = new HandlerList();
